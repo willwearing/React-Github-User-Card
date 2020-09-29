d091ecd91ae5ca7ac40c2ee134e345cea7247dac
willwearing <willwearing@gmail.com>
2020-09-29 13:56:05 -0600

    setup usercard - working, got follower data in app - working as well

--- src/App.js
+++ src/App.js
@@ -2,13 +2,55 @@ import React from "react";
 import UserCard from "./components/UserCard";
 import "./App.css";
 
+const userName = "willwearing";
+
 class App extends React.Component {
-  state = {};
+  state = {
+    user: [],
+    followers: [],
+  };
+
+  componentDidMount() {
+    this.fetchUser(this.state.user);
+    this.fetchFollowers(this.state.followers);
+  }
+
+  fetchFollowers = () => {
+    fetch(`https://api.github.com/users/${userName}/followers`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          followers: data,
+        });
+      })
+      .catch((err) => {
+        console.log("follower fetch error", err);
+      });
+  };
+
+  fetchUser = (user) => {
+    fetch(`https://api.github.com/users/${userName}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        this.setState({
+          user: data,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>GitHub Users of People we Like</h1>
-        <UserCard />
+        <UserCard user={this.state.user} />
       </div>
     );
   }
--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const UserCard = (props) => {
+const UserCard = ({ user }) => {
   return (
     <div className="userCard">
-      <h1>This is our usercard</h1>
+      <h2>Username: {user.name}</h2>
+      <img width="200px" src={user.avatar_url} alt={user.name} />
+      <p>Bio: {user.bio}</p>
+      <p>Location: {user.location}</p>
+      <p># of followers: {user.followers}</p>
     </div>
   );
 };
